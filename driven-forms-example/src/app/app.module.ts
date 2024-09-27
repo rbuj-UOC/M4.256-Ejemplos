25437e3d7f50c597f5d26e13b06b78da83738d5a
Robert Buj <robert.buj@gmail.com>
2024-09-28 01:25:49 +0200

    Add component login
    ng g component components/login --skip-tests=true

--- driven-forms-example/src/app/app.module.ts
+++ driven-forms-example/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, LoginComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
