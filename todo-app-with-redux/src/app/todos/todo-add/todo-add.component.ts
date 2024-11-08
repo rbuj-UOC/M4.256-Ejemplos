a227331404b0ee6b06b4847ad55c28edd5c5247c
Robert Buj <robert.buj@gmail.com>
2024-11-08 23:05:59 +0100

    todo-app-with-redux: TodoAddComponent

--- todo-app-with-redux/src/app/todos/todo-add/todo-add.component.ts
+++ todo-app-with-redux/src/app/todos/todo-add/todo-add.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../app.reducer';
+import { createTodo } from '../todo.actions';
 
 @Component({
   selector: 'app-todo-add',
@@ -6,5 +10,16 @@ import { Component } from '@angular/core';
   styleUrl: './todo-add.component.scss'
 })
 export class TodoAddComponent {
+  public titleInput: FormControl;
 
+  constructor(private store: Store<AppState>) {
+    this.titleInput = new FormControl('', Validators.required);
+  }
+
+  addTodoTask() {
+    if (this.titleInput.valid) {
+      this.store.dispatch(createTodo({ title: this.titleInput.value }));
+      this.titleInput.reset();
+    }
+  }
 }
--- todo-app-with-redux/src/app/todos/todo.module.ts
+++ todo-app-with-redux/src/app/todos/todo.module.ts
@@ -1,19 +1,13 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TodoListComponent } from './todo-list/todo-list.component';
-import { TodoListItemComponent } from './todo-list-item/todo-list-item.component';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { TodoAddComponent } from './todo-add/todo-add.component';
-
-
+import { TodoListItemComponent } from './todo-list-item/todo-list-item.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
 @NgModule({
-  declarations: [
-    TodoListComponent,
-    TodoListItemComponent,
-    TodoAddComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [TodoListComponent, TodoListItemComponent, TodoAddComponent],
+  imports: [CommonModule, ReactiveFormsModule],
+  exports: [TodoListItemComponent, TodoListComponent, TodoAddComponent]
 })
-export class TodoModule { }
+export class TodoModule {}
