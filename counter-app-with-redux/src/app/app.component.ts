e754ec70e8ee9de022294309791a854873e59572
Robert Buj <robert.buj@gmail.com>
2024-11-08 19:24:53 +0100

    counter-app-with-redux: Mother

--- counter-app-with-redux/src/app/app.component.ts
+++ counter-app-with-redux/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { decrement, increment } from './Counter/counter.actions';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'counter-app-with-redux';
+  counter!: number;
+
+  constructor(private store: Store<{ counter: number }>) {
+    // this.counter = 20;
+    this.store.subscribe((state) => {
+      this.counter = state.counter;
+    });
+  }
+
+  increase(): void {
+    // this.counter = this.counter + 1;
+    this.store.dispatch(increment());
+  }
+
+  decrease(): void {
+    //this.counter = this.counter - 1;
+    this.store.dispatch(decrement());
+  }
 }
--- counter-app-with-redux/src/app/app.module.ts
+++ counter-app-with-redux/src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { StoreModule } from '@ngrx/store';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { counterReducer } from './Counter/counter.reducer';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({ counter: counterReducer })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
