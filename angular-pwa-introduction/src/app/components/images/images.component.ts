4399916203de53fc40897fa326516a88ef13e58e
Robert Buj <robert.buj@gmail.com>
2024-12-29 16:38:15 +0100

    angular-pwa-introduction: console.log(images)

---
+++ angular-pwa-introduction/src/app/components/images/images.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { ImagesService } from '../../services/images.service';
+import { Image } from '../../models/image.interface';
+
+@Component({
+  selector: 'app-images',
+  templateUrl: './images.component.html',
+  styleUrl: './images.component.css'
+})
+export class ImagesComponent implements OnInit {
+  constructor(private imagesService: ImagesService){};
+  ngOnInit(): void {
+    this.imagesService.getAllImages().subscribe((images) => console.log(images));
+  }
+}
---
+++ angular-pwa-introduction/src/app/models/image.interface.ts
@@ -0,0 +1,8 @@
+export interface Image {
+  id: string;
+  author: string;
+  width: number;
+  heigh: number;
+  url: string;
+  download_url: string;
+}
