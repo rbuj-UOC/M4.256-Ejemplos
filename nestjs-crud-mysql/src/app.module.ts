d710bf8a112360652453ca01f5a4fabde18e17c4
Robert Buj <robert.buj@gmail.com>
2025-02-14 12:14:49 +0100

    nest g res products

--- nestjs-crud-mysql/src/app.module.ts
+++ nestjs-crud-mysql/src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { ProductsModule } from './products/products.module';
 import databaseConfig from 'config/database.config';
 
 @Module({
-  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot(databaseConfig())],
+  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot(databaseConfig()), ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
