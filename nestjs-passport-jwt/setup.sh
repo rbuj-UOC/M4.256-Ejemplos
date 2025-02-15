b9051f0de18acbca6d858f3d7e54755fd726b79a
Robert Buj <robert.buj@gmail.com>
2025-02-15 10:32:43 +0100

    nestjs-passport-jwt: passport

---
+++ nestjs-passport-jwt/setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+PROJECT_NAME=nestjs-passport-jwt
+
+# create the project
+npx @nestjs/cli@latest new ${PROJECT_NAME} --package-manager npm --skip-git
+
+cd ${PROJECT_NAME}
+
+npm install --save @nestjs/typeorm typeorm pg
+
+npm install --save @nestjs/config
+
+npm install --save @nestjs/class-validator class-validator
+
+npm install --save @nestjs/passport passport passport-local
+npm install --save-dev @types/passport-local
+
+npm install --save @nestjs/jwt passport-jwt
+npm install --save-dev @types/passport-jwt
+
+# nest g module auth
+# nest g service auth --no-spec
+# nest g module users
+# nest g service users --no-spec
---
+++ nestjs-passport-jwt/src/app.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Request, Post, UseGuards } from '@nestjs/common';
+import { LocalAuthGuard } from './auth/local-auth.guard';
+
+@Controller()
+export class AppController {
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    return req.user;
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/logout')
+  async logout(@Request() req) {
+    return req.logout();
+  }
+}
