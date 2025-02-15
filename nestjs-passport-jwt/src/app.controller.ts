946f2fbe043b158657a74bd1183fde09b1db4d1d
Robert Buj <robert.buj@gmail.com>
2025-02-15 10:56:51 +0100

    nestjs-passport-jwt: passport jwt

--- nestjs-passport-jwt/src/app.controller.ts
+++ nestjs-passport-jwt/src/app.controller.ts
@@ -1,17 +1,21 @@
-import { Controller, Request, Post, UseGuards } from '@nestjs/common';
+import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthService } from './auth/auth.service';
 
 @Controller()
 export class AppController {
+  constructor(private authService: AuthService) {}
+
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
-    return req.user;
+    return this.authService.login(req.user);
   }
 
-  @UseGuards(LocalAuthGuard)
-  @Post('auth/logout')
-  async logout(@Request() req) {
-    return req.logout();
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
   }
 }
